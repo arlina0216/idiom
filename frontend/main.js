87a3285504e78360a71fd343606dee95164b1a2f
arlina0216 <arlina0216@gmail.com>
2026-03-19 08:40:41 +0800

    Update API_BASE URL for backend service

--- frontend/main.js
+++ frontend/main.js
@@ -23,7 +23,7 @@
   var API_BASE =
     window.location.protocol === 'http:' || window.location.protocol === 'https:'
       ? ''
-      : 'https://idiom-backend-fg74.onrender.com';
+      : 'https://idiom-backend.onrender.com/';
 
   // 畫板：畫線
   var drawing = false;
